6be8937f73f821ba91c757c0a1c82701a1b29a0b
wenyiqing <wenyiqing@tencent.com>
2025-03-28 10:32:06 +0800

    删除i18

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,11 @@ import router from './router';
 import 'tdesign-vue/es/style/index.css';
 import { Dialog,  Input, Alert} from 'tdesign-vue';
 import { createPinia, PiniaVuePlugin } from 'pinia';
-import i18n from './locales';
 
 Vue.use(PiniaVuePlugin);
 const pinia = createPinia();
 
 // @ts-ignore
-Vue.use(i18n);
 Vue.use(Dialog);
 Vue.use(Input);
 Vue.use(Alert);
